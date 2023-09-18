54e90f07d84ac180a854157b7b436adafef38a30
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-09-18 16:32:02 -0400

    FULL JAVASCRIPT FUNCTIONALITY ACHIEVED.

--- js/get.js
+++ js/get.js
@@ -4,6 +4,10 @@ async function getWithAwait(url) {
     try {
         const response = await fetch(url);
         const data = await response.json();
+        // CALL THE FUNCTION TO DISPLAY DATA ON SCREEN BC DATA LIVES HERE IN SCOPE
+
+        displayInfo(data);
+
         return data;
     } catch (error) {
         console.error('Error In Try/Catch: ', error);
@@ -11,15 +15,18 @@ async function getWithAwait(url) {
 };
 
 
-// function get(url) {
-//     return fetch(url)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             return data;
-//         })
-//         .catch(function (error) {
-//             console.log("Error: ", error);
-//         })
-// };
+// A FUNCTION TO DISPLAY THE DATA INTO THE INNERTEXT I WANT TO
+
+function displayInfo(a) {
+    console.log(a);
+    const selectNameEl = document.getElementById('returnName');
+    const selectUserNameEl = document.getElementById('returnUserName');
+    const selectReposEl = document.getElementById('returnRepos');
+    const selectFollowersEl = document.getElementById('returnFollowers');
+    const selectFollowingEl = document.getElementById('returnFollowing');
+    selectNameEl.innerText = a.name;
+    selectUserNameEl.innerText = a.login;    
+    selectReposEl.innerText = a.public_repos;
+    selectFollowersEl.innerText = a.followers;
+    selectFollowingEl.innerText = a.following;
+};
--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 'use strict'
 
+const gitUserInfo = document.querySelector('#returnInfo');
+
+// const returnInfo = getWithAwait(apiUrl);
+//     showInfo(returnInfo.value, gitUserInfo);
+
 const gitHubFormEl = document.querySelector('#gitHubForm');
 
-document.addEventListener("DOMContentLoaded", async function() {
-    console.log("DOM IS READY. HAVE A CORONA, FAMILY.");
+document.addEventListener("DOMContentLoaded", async function () {
+    console.log("DOM IS READY. HAVE A CORONA.  IN HERE, WE FAMILY.");
 
-    const apiUrl = 
+    const submitButton = document.getElementById('btn1');
+
+    submitButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        const newUser = document.getElementById('newUser').value;
+        const apiUrl = `https://api.github.com/users/${newUser}`
+        console.log(apiUrl);
+        console.log(newUser);
+        getWithAwait(apiUrl);
+        // document.getElementById('#returnInfo')
+    });
 });
